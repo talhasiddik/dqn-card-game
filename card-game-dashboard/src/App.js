3d9c4b0af34a502278d4e9c2cbd517d1cd76a0e0
Muhammad Talha Siddique <153253462+talhasiddik@users.noreply.github.com>
2025-04-22 15:07:51 +0500

    add files

---
+++ card-game-dashboard/src/App.js
@@ -0,0 +1,15 @@
+import './App.css';
+import './index.css';
+
+import React from 'react';
+import Dashboard from './components/Dashboard';
+
+function App() {
+  return (
+    <div className="App">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
